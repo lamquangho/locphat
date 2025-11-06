31707b9295cdc7af544a86d12bf56f9ca9668fcd
lamquangho <holam24062003@gmail.com>
2025-11-06 17:19:48 +0700

    update2

--- boc-xep-website/backend/config/db.js
+++ boc-xep-website/backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://holam24062003_db_user:quangho123@cluster0.bpw0vps.mongodb.net/bocxep', {
+    const conn = await mongoose.connect('mongodb+srv://holam24062003_db_user:quangho123@cluster0.bpw0vps.mongodb.net/bocxep' || process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
